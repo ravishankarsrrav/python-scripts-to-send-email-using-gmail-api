1f83fc7501ccb9fe73ee809e1dcac5ae5f7a6254
ravishankarsr <rr@zaubacorp.com>
2019-07-10 15:27:36 +0530

    added support for parsing excel sheets and sending emails

--- send-email.py
+++ send-email.py
@@ -5,7 +5,18 @@ from googleapiclient.discovery import build
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 from oauth2client import tools
+import xlrd
 
+
+#Path of excel sheet
+EXCEL_SHEET_PATH = ("test.xlsx")
+
+#work book of excel
+WorkBook = xlrd.open_workbook(EXCEL_SHEET_PATH) 
+
+#excel sheet
+Excel_Sheet = WorkBook.sheet_by_index(0) 
+Excel_Sheet.cell_value(0, 0)
 
 # Path to the client_secret.json file downloaded from the Developer Console
 CLIENT_SECRET_FILE = 'credentials.json'
@@ -31,17 +42,30 @@ http = credentials.authorize(http)
 # Build the Gmail service from discovery
 gmail_service = build('gmail', 'v1', http=http)
 
-# create a message to send
-message = MIMEText("Hello Ravi,\r\n\r\n It's amazing you can send email using scripts.\r\n\r\n ")
-message['to'] = "replaceto@gmail.com" #replace to email Id
-message['from'] = "replacefrom@gmail.com" #replace from email Id
-message['subject'] = "Reporting cool stuff" #replace the subject
-body = {'raw': base64.b64encode(message.as_string())}
 
-# send it
-try:
-  message = (gmail_service.users().messages().send(userId="me", body=body).execute())
-  print('Message Id: %s' % message['id'])
-  print(message)
-except Exception as error:
-  print('An error occurred: %s' % error)
+for i in range(0, Excel_Sheet.nrows):
+  name_of_candidate = Excel_Sheet.row_values(i)[0]
+  print(name_of_candidate)
+  text_msg = """\
+Hey """+ name_of_candidate +""" ,
+
+Thanks.
+
+Regards,
+Ravi    
+"""
+
+  # create a message to send
+  message = MIMEText(text_msg)
+  message['to'] = Excel_Sheet.row_values(i)[1] #replace to email Id
+  message['from'] = "Zauba Cloud <akarsh@zauba.company>" #replace from email Id
+  message['subject'] = "Zauba Cloud Interview - Schedule Request" #replace the subject
+  body = {'raw': base64.urlsafe_b64encode(message.as_string().encode()).decode()}
+
+  # send it
+  try:
+    message = (gmail_service.users().messages().send(userId="me", body=body).execute())
+    print('Message Id: %s' % message['id'])
+    print(message)
+  except Exception as error:
+    print('An error occurred: %s' % error)
